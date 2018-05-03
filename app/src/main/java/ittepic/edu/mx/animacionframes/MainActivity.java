f445b137fb0cba231422c011775f8e47ed009e8f
Zulma <zuiscoronello@ittepic.edu.mx>
2018-05-03 17:45:54 -0600

    Initial commit

---
+++ app/src/main/java/ittepic/edu/mx/animacionframes/MainActivity.java
@@ -0,0 +1,40 @@
+package ittepic.edu.mx.animacionframes;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+    private AnimationDrawable blackMen;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+        if (imageView == null) throw new AssertionError();
+
+        imageView.setVisibility(View.INVISIBLE);
+        imageView.setBackgroundResource(R.drawable.animation_list);
+
+        blackMen = (AnimationDrawable) imageView.getBackground();
+        blackMen.setOneShot(true);
+    }
+
+    public void onStartBtnClick(View v) {
+        imageView.setVisibility(View.VISIBLE);
+        if (blackMen.isRunning()) {
+            blackMen.stop();
+        }
+        blackMen.start();
+    }
+
+    public void onStopBtnClick(View v) {
+        blackMen.stop();
+    }
+
+}
